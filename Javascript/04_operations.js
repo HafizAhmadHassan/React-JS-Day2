7cd6e86f7c8b770068e1e376fefc030f18823518
Hafiz Ahmad Hassan <31056702+HafizAhmadHassan@users.noreply.github.com>
2025-01-08 15:32:33 +0100

    Javascript

---
+++ Javascript/04_operations.js
@@ -0,0 +1,22 @@
+let value = 3;
+let negatedValue = -value;
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+
+let str1="Hello"
+let str2="World"
+
+let str3=str1+str2
+
+console.log(str3)
+
+console.log("1" + 2)
+console.log("1" + 2 + 2) // 122
+console.log(2 + 2 + "1") // 41 How it is 41 because it is left to right ECMA Script
+
+/* Use parenthesis when writing code  */ 
+console.log((2 + 2) * 5 % 3) // 4 * 5 % 3 => 20 % 3 => 2
+console.log(+true) // 1
+console.log(+"") // 0
